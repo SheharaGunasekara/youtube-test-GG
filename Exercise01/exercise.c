1ea461ccfb56bc6428c99f3fee2eaf923bfebb37
Shehara Gunasekara <k.sheharagunasekara@gmail.com>
2026-05-06 10:25:58 +0530

    mama gay

---
+++ Exercise01/exercise.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+	
+	//3.Declare age = 21, height 5.7, grade = 'A' and print all in new line.
+	
+	int age = 21;
+	float height = 5.7;
+	char grade = 'A';
+	
+	printf("\n\nAge : %d \n\nHeight : %.2f \n\nGrade : %c \n\n", age,height,grade);
+	
+	//4.Declare two integers a = 4, b = 6, and prnt their sum and product.
+	
+	int a = 4;
+	int b = 6;
+	
+	printf("Sum : %d \n\n", a+b);
+	
+	//5.Declare int x = 2, y = 3, z = 5 and print them in a single printf().
+	
+	int x = 2;
+	int y = 3;
+	int z = 5;
+	
+	printf("%d , %d , %d \n\n", x, y, z);
+	
+
+	
+	return 0;
+}
